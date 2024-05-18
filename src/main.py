586347093959144f00fb396abe130b6a8027d3b8
dbaitech <diana.brebeanu@gmail.com>
2024-05-18 15:53:43 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -13,49 +13,55 @@ from dotenv import load_dotenv
 from llama_cpp import Llama
 
 load_dotenv()
-# openai_client = OpenAI(
-#     api_key=os.environ.get("OPENAI_API_KEY"),
-# )
+openai_client = OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+
 def get_prompt_categories(prompt):
     categories = Categories().to_dict()
-    llm = Llama(model_path="./models/7B/llama-model.gguf")
-    response = llm("Q: Name the planets in the solar system? A: ", max_tokens=32, stop=["Q:", "\n"], echo=True)
-    # response = openai_client.completions.create(
-    #     model="gpt-3.5-turbo",
-    #     prompt=f"Extract the categories from the text and assign them into the JSON object's categories: {prompt}\n\n"
-    #            f"Categories: {categories}\n\n",
-    # )
+    # llm = Llama(model_path="./models/7B/llama-model.gguf")
+    # response = llm("Q: Name the planets in the solar system? A: ", max_tokens=32, stop=["Q:", "\n"], echo=True)
+    response = openai_client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user",
+             "content": f"Extract the categories from the following text and assign them into the JSON object's "
+                        f"categories. Be careful to place any movie titles in the appropriate category, not just "
+                        f"in with_keywords. Return only the given JSON object with filled in values."
+                        f"{prompt}\n\n"
+                        f"Categories: {categories}\n\n"},
+        ]
+    )
     return response
+
 
 prompt = "I want to watch a movie similar to The Imitation Game so that I can be inspired."
 ai_response = get_prompt_categories(prompt)
-print(ai_response)
-# nlp = spacy.load("en_core_web_sm")
-# doc = nlp(prompt)
-# # relevant_tokens = []
-# for token in doc:
-#     print(token.ent_type_)
-#     if token.pos_ in ['NOUN', 'PROPN'] or token.ent_type_ == 'DATE':
-#         relevant_tokens.append(token.text)
-#
-# print("Relevant tokens:", relevant_tokens)
+content = json.loads(ai_response.choices[0].message.content)
+categories = content['categories']
 
 base_url = 'https://api.themoviedb.org/3'
 api_client = APIClient(base_url)
 
-# search movie
-title = "The Imitation Game"
-movie = MovieSearchResult(**api_client.search_movie(title))
+similar_movie_genre_id_str = []
+similar_movie_keyword_id_str = []
+# search movie if mentioned by user
+if categories['movie_title']:
+    title = categories['movie_title']
+    similar_movie = MovieSearchResult(**api_client.search_movie(title))
 
-genre_ids = movie.genre_ids
-keywords = api_client.get_movie_keywords(movie.id)
+    similar_movie_genre_ids = similar_movie.genre_ids
+    similar_movie_keywords = api_client.get_movie_keywords(similar_movie.id)
 
-keyword_id_str = int_list_to_str(keywords, 'id')
-genre_id_str = int_list_to_str(genre_ids)
+    similar_movie_genre_id_str = int_list_to_str(similar_movie_genre_ids)
+    similar_movie_keyword_id_str = int_list_to_str(similar_movie_keywords, 'id')
+
 
 params = DiscoverMoviesParams()
-params.with_keywords = keyword_id_str
-params.with_genres = genre_id_str
+params.with_genres += similar_movie_genre_id_str
+params.with_keywords += similar_movie_keyword_id_str
 
 params = flatten_dict(params.__dict__)
 recommended_movies = api_client.get_similar_movies(params)
